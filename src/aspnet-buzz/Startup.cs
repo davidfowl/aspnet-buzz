f4ec2de9928db0283a1199c8dcffbfc6c62c1f1d
David Fowler <davidfowl@gmail.com>
2015-04-11 18:48:48 -0700

    Added some codez - Initial NuGet.Config (using bleeding edge bits) - Added custom data protection

---
+++ src/aspnet-buzz/Startup.cs
@@ -0,0 +1,22 @@
+using System;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.DependencyInjection;
+
+namespace AspNet.Buzz
+{
+    public class Startup
+    {
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseStaticFiles();
+            app.UseSignalR();
+        }
+    }
+}
