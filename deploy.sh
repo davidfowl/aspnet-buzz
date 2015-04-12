27796fb4453480cf41e3e95486608913cc8971c9
David Fowler <davidfowl@gmail.com>
2015-04-12 03:16:07 -0700

    Run git submodule update in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 rm -rf artifacts
+git submodule update --init
 if ! type dnvm > /dev/null 2>&1; then
     curl -sSL https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.sh | DNX_BRANCH=dev sh && source ~/.dnx/dnvm/dnvm.sh
 fi
