cc343f750bee0614b7336701cb582b0a8ae8db6a
David Fowler <davidfowl@gmail.com>
2015-04-14 04:33:52 -0700

    Updated deploy.sh to manually restore folders

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,9 @@ if ! type dnvm > /dev/null 2>&1; then
     curl -sSL https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.sh | DNX_BRANCH=dev sh && source ~/.dnx/dnvm/dnvm.sh
 fi
 dnvm upgrade
-dnu restore
+dnu restore src/DataProtection
+dnu restore src/SignalR-Server
+dnu restore src/aspnet-buzz
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 dnu publish src/aspnet-buzz --no-source --out artifacts/build/aspnet-buzz 2>&1 | tee buildlog
 # work around for kpm bundle returning an exit code 0 on failure 
